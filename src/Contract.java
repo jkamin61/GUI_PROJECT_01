21f6d2ac7d37d6110b14a5d85fe65f50cb30eefc
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2024-04-20 11:08:22 +0200

    added required constructors, changed date to localdatetime, added method endContract, getContractStatus

--- src/Contract.java
+++ src/Contract.java
@@ -1,18 +1,54 @@
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
 class Contract implements Runnable {
+
+    ContractStatus contractStatus;
     private static int numberOfContracts = 0;
     private int contractId;
-    private Map<Integer, Job> jobs;
-    private String foreman;
-    private Map<String, String> employeesDepartment;
+    Map<Integer, Job> jobs;
+    String foreman;
+    Map<String, String> employeesDepartment;
+    Brigade brigade;
+    boolean isBrigadeAssigned = false;
+    LocalDateTime dateOfContractCreation;
+    LocalDateTime dateOfStart;
+    LocalDateTime dateOfEnd;
+    boolean isStarted = false;
+    boolean isFinished = false;
 
-    public Contract(String foreman) {
+    public Contract(String foreman, Brigade brigade, LocalDateTime dateOfContractCreation) {
         this.contractId = ++numberOfContracts;
         this.jobs = new HashMap<>();
         this.foreman = foreman;
         this.employeesDepartment = new HashMap<>();
+        this.brigade = brigade;
+        this.isBrigadeAssigned = true;
+        this.dateOfContractCreation = dateOfContractCreation;
+        this.dateOfStart = LocalDateTime.now();
+    }
+
+    public Contract(boolean isPlanned) {
+        this.contractStatus = isPlanned ? ContractStatus.PLANNED : ContractStatus.NOT_PLANNED;
+    }
+
+    public Contract(Brigade brigade, boolean isPlaned) {
+        this.brigade = brigade;
+        this.isBrigadeAssigned = true;
+        this.contractStatus = isPlaned ? ContractStatus.PLANNED : ContractStatus.NOT_PLANNED;
+    }
+
+    public Contract(HashMap<Integer, Job> jobs, boolean isPlaned) {
+        this.jobs = jobs;
+        this.contractStatus = isPlaned ? ContractStatus.PLANNED : ContractStatus.NOT_PLANNED;
+    }
+
+    public Contract(HashMap<Integer, Job> jobs, boolean isPlanned, Brigade brigade) {
+        this.jobs = jobs;
+        this.contractStatus = isPlanned ? ContractStatus.PLANNED : ContractStatus.NOT_PLANNED;
+        this.brigade = brigade;
+        this.isBrigadeAssigned = true;
     }
 
     public void addJob(Job job) {
@@ -27,6 +63,15 @@ class Contract implements Runnable {
         return this.foreman.equals(foreman);
     }
 
+    public static void endContract(Contract contract) {
+        contract.isFinished = true;
+        contract.dateOfEnd = LocalDateTime.now();
+    }
+
+    public static ContractStatus getContractStatus(Contract contract) {
+        return contract.contractStatus;
+    }
+
     @Override
     public String toString() {
         return "Contract number: " + contractId +
@@ -37,5 +82,6 @@ class Contract implements Runnable {
 
     @Override
     public void run() {
+
     }
 }
